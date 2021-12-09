a72a77f3ccf164cef9ffa9feccebdfad63e2b719
Rene Marino <renemarino93@gmail.com>
2021-12-09 15:21:06 -0800

    first_commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>Things I need to do:</h2>
+      <li>Learn React</li>
+      <li>CLimb Mt. Everest</li>
+      <li>Feed the dogs</li>
+    </div>
+  );
+}
+
+export default App;
